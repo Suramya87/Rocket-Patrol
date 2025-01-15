39c33dff27cccb5c82464e9e68bd098c51c99c21
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-01-15 09:02:04 -0800

    save point

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@
 // Name: Suramya Shakya
 // Date: 01/13/2025
 
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+  }
 
-console.log("hello world")
+let game = new Phaser.Game(config)
+
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
