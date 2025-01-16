03729fd75b82ac11f6f88f9ae8861fef9180dec3
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-01-15 23:49:29 -0800

    ships plus killing

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,7 +2,6 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame)
-  
       // add object to existing scene
       scene.add.existing(this)
       this.isFiring = false
@@ -16,15 +15,19 @@ class Rocket extends Phaser.GameObjects.Sprite {
           this.x += this.moveSpeed
         }
       }
-      if (Phaser.Input.KeyBorad.JustDown(keyFIRE)) {
+      if (Phaser.Input.Keyboard.JustDown(keyFIRE)) {
         this.isFiring = true
       }
-      if (this.isFiring &&this.y >= borderUISize * 3 + borderPadding) {
+      if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
         this.y -= this.moveSpeed
       }
       if(this.y <= borderUISize * 3 + borderPadding) {
         this.isFiring = false
         this.y = game.config.height - borderUISize - borderPadding
       }
+    }
+    reset() {
+      this.isFiring = false
+      this.y = game.config.height - borderUISize - borderPadding
     }
   }
